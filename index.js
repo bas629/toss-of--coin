1b294fa92bc8518ca32c4ea1f055aed9547e0934
bas629 <basupahariya6@gmail.com>
2023-07-19 14:21:32 +0530

    Second commit

---
+++ index.js
@@ -0,0 +1,84 @@
+
+// let a=["basu" ,"sagar","sawan","sakshi"]; 
+
+// // let c=document.getElementById("boss"); 
+// // c.onclick=tellname();
+// function tellname( )
+// {let n= Math.random()*4;  
+// n=Number.parseInt(n);
+//   let c=document.getElementById("demo")
+//    c.innerHTML=a[n];
+// }  
+
+// let a=<img src="https://picsum.photos/200/300"/ >
+// coin.innerHTML=a;
+
+// var img = document.createElement("img");
+
+// img.src ="https://en.wikipedia.org/wiki/Image#/media/File:Image_created_with_a_mobile_phone.png;
+
+// var src = document.getElementById("myImg");
+
+// src.appendChild(img);
+
+//  
+
+// button1.addEventListener("click" , function(e) {
+// alert("button1 clicked"); 
+
+
+// })
+// button2.addEventListener("click" , function(e) {
+//     alert("button2 clicked"); 
+    
+    
+//     }) 
+
+//     button3.addEventListener("click" , function(e) {
+    
+//     window .location="https://www.google.co.in/webhp";
+
+
+//     })
+//     button4.addEventListener("click" , function(e) {
+    
+//         window .location="https://facebook.com/webhp";
+    
+    
+//         })
+//         button5.addEventListener("click" , function(e) {
+    
+//             window .location="https://twitter.com/i/flow/login?redirect_after_login=%2Flogin%3Flang%3Den";
+        
+        
+//             })
+
+// setInterval(async function(){
+// document.querySelector("#krishna").classList.toggle("tail");
+
+
+
+// },300)  
+
+// function myfunction(){
+//  let a=["head","tail"] 
+//  let n=Math.random()*2; 
+//  n=Number.parseInt(n);
+
+
+//     document.querySelector("#krishna").classList.remove(a[n]);
+
+
+// }
+
+ 
+let a=["Rohan & Mohan were sitting in a kabristan & were talkingRohan: Mohan dekho yeh murde kitne aaraam se apni kabron mein sote hai.Sare murde uth khare hue aur bole Kyun na soye yeh jaga tari bapa ki thodhi na hai!","Rubel apni lover se milne gaya, kuch baat ke baad uski girlfirend chaye(tea) banane ke liye rannaghar pe chali gayi,Lover ke mobile sofa pe dekh kar Rubel ne socha ke chalo dekhte hai mera number iss ne kisnaam se save kya hai?Dear, sweety ya jaanu!Jab usne misscall di to screen pe likh raha tha “Murga No.5? Calling!!!","Parul (naukrani) ne Parul se kaha, memsahab gajab ho gaya, pados ki teen auraten aap ki saas ko peet rahi hai.Parul Naukrani ke saath balakani se aayi aur chup chap tamasha dekhne lagi.Naukarani ne pucha, aap madad karne nahi jayengi?Parul: Nahi uske liye teen hi kafi hai.","Habildar: Tumhe kal subah 6 baje pe phansi di jayegi Liton: Ha ha ha!Habildar: Kyu hass rahe ho?Liton: Main toh subha 9 baje tak sota hoon!","Pappu: yar mere pas paise nhi hy mujhe kapre lena hen kya karun?Dost: To bank se loan le loPappu: Loan to le lun magar hmko sirf cotton pehanne ki adat hai"]
+
+
+ function myfunction()
+ {
+    let n=Math.random()*5; 
+  n=Number.parseInt(n); 
+   joke.innerHTML=a[n];
+ 
+}  
